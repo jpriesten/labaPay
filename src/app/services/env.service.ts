581e9af9f121ff4d6a0d1e360561593f62c2544e
Jezeh Priesten <jezeh.priesten@pethersolutions.com>
2019-07-14 20:53:53 +0100

    Updated source of API

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
-  // https://labapay-server.herokuapp.com/
-  API_URL = 'http://localhost:3000/';
+  // https://labapay-server.herokuapp.com/  http://localhost:3000/
+  API_URL = 'https://labapay-server.herokuapp.com/';
   
   constructor() { }
 }
