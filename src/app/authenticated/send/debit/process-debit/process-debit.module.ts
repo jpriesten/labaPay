9558184f8e67c58ce2eabd265ff9537dec5cefef
Jezeh Priesten <jezeh.priesten@pethersolutions.com>
2019-08-20 10:25:12 +0100

    Fixed declaration issue

--- src/app/authenticated/send/debit/process-debit/process-debit.module.ts
+++ src/app/authenticated/send/debit/process-debit/process-debit.module.ts
@@ -21,6 +21,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [ProcessDebitPage]
+  declarations: []
 })
 export class ProcessDebitPageModule {}
